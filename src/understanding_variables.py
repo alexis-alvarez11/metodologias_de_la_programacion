1c9fced4d9430d9bce9827919d63428cd4777648
ALEXIS <2530141@upv.edu.mx>
2025-10-27 09:53:07 -0600

    feat: first class

---
+++ src/understanding_variables.py
@@ -0,0 +1,16 @@
+message = "This is my first python variable"
+another_message ='I am really, really really happy'
+
+#print () -> use to show message in terminal
+#print () -> se utiliza para mostrar mensajes en la terminal
+
+print(message)
+print(another_message)
+print(message, another_message, message)
+print(another_message, message)
+
+message = "I love Python"
+print(message)
+
+alexis_message = "Hola soy alexis, y aprendo python"
+print(alexis_message)
