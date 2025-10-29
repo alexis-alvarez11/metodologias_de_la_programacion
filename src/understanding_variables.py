b096b2994c10a51892f6baa43c4e6af03f02ffdd
ALEXIS <2530141@upv.edu.mx>
2025-10-29 09:28:41 -0600

    feat: traceback

--- src/understanding_variables.py
+++ src/understanding_variables.py
@@ -14,3 +14,19 @@ print(message)
 
 alexis_message = "Hola soy alexis, y aprendo python"
 print(alexis_message)
+
+"""
+Traceback: Es un registro donde el interprete tuvo problemas al intentar ejecutar su código.
+
+Ejemplo: 
+Traceback (most recent call last):
+ File "C:/Users/Acer/Desktop/projects/metodologias_de_la_programacion/src/understanding_variables.py", line 17, in <module>
+    print(alexis_mesage)
+          ^^^^^^^^^^^^^
+NameError: name 'alexis_mesage' is not defined. Did you mean: 'alexis_message'?
+
+name error: significa que olvdamos establecer el valor de una variable antes de utilizar o 
+cometimos un error al ingresar el nombre de la variable.
+
+
+"""
